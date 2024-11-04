0c0bafa6d5ff47cebcb5f6415f31dac533e78ece
Miruthula N <mirthula@>
2024-11-04 18:00:37 +0530

    initial style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: [],
   theme: {
     extend: {},
+    fontSize: {
+      "12px": "12px",
+      "18px": "18px",
+      "16px": "16px",
+      "14px": "14px",
+      "20px": "20px",
+      "26px": "26px",
+      "45px": "45px",
+    },
   },
   plugins: [],
 }
